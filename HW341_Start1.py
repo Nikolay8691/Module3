b4cc36bb8e98d82318c46079c61f07d404dec83f
Nikolay V <nv-mail@mail.ru>
2022-04-24 10:30:02 +0300

    Module3 Part4 Homework1 - Create Start file for main program

---
+++ HW341_Start1.py
@@ -0,0 +1,8 @@
+print (' welcome to Module 3 Part 4 homework 1! - Start Part')
+import json
+
+users = {'startlogin' : 'brunoym123'}
+datafile = 'users_data1.json'
+
+with open(datafile, 'w') as f:
+	json.dump(users,f)
