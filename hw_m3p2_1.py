d91b7dd6027381bdb5b55e7d358a64ecaf249624
LAPTOP-FMOIC7M3\nv-ma <nv-mail@mail.ru>
2023-07-25 10:14:35 +0300

    replaced for loop into while as searching for repetion

--- hw_m3p2_1.py
+++ hw_m3p2_1.py
@@ -1,6 +1,6 @@
 print(' welcome to Module 3 Part 2 homework! ')
 
-l = [1, 4, 1, 6, 'hello', 'a', 5, 'hello']
+l = [1, 4, 1, 6, 'hello', 'a', 1, 'hello']
 
 l_unique = []
 for i in l:
@@ -22,4 +22,20 @@ for i in l_unique :
 			if j > 1 :
 				l_repeat.append(i)
 
+l_repeat_unique = []
+l_repeat_unique = list(set(l_repeat))
+print('repeated elements of the list : ', l_repeat_unique)
+
 print('repeated elements of the list : ', l_repeat)
+
+l_repeat2 = []
+for i in l_unique :
+	j, k = 0, 0
+	while j <= 1 and k < len(l):
+		if i == l[k] :
+			j += 1
+			if j > 1:
+				l_repeat2.append(i)
+		k += 1
+
+print('repeated_2 elements of the list : ', l_repeat2)
